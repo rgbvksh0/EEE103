bdf2e15bf6c4a942f0064a2ad0925ecb62d8feff
rgbvksh0 <ahmad.sayeed.baksh@g.bracu.ac.bd>
2026-07-08 13:44:25 +0600

    08.07

---
+++ 08.07/break.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+
+// didnt do it, cuz lazy!! USE THE BREAK STATEMENT TO DO IT
+int main(){
+
+int n1;
+int n2;
+printf("enter the range:\n");
+scanf("%d", &n1);
+scanf("%d", &n2);
+
+int i,j;
+
+for(i=n1; i<=n2; i=i+1)
+{
+    int count=0;
+    for(j=1; j<=i; j=j+1)
+    {
+        if(i%j==0)
+        {
+            count=count+1;
+        }
+    }
+    if(count==2)
+    {
+        printf("%d\n",i);
+    }
+}
+
+
+return 0;
+}
