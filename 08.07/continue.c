6f0c389eb5959be83a05ab5cddbabb9891746847
rgbvksh0 <ahmad.sayeed.baksh@g.bracu.ac.bd>
2026-07-08 13:48:16 +0600

    08.07

---
+++ 08.07/continue.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+
+int main()
+{
+    for(int i=1; i<=10; i++)
+    {
+        if (i==5||i==8)
+        {
+            continue:
+        }
+
+        printf("%d\n", i);
+    }
+}
+
+
+return 0;
+}
