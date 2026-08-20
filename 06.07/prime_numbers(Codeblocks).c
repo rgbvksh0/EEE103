a6f137d8c9cf289890908d45080e1ad7f8a43632
BVKSH0 <153601963+BVKSH0@users.noreply.github.com>
2026-08-20 08:28:12 +0600

    20.08

--- 06.07/prime_numbers(Codeblocks).c
+++ 06.07/prime_numbers(Codeblocks).c
@@ -24,3 +24,40 @@ if(count==2)
 };
 return 0;
 }
+
+
+#include <stdio.h>
+/*
+int prime() {
+    int n;
+    printf("Enter the num:\n");
+    if (scanf("%d", &n) != 1) {
+        printf("Invalid input.\n");
+        return 1;
+    }
+
+    // Numbers less than or equal to 1 are not prime
+    if (n <= 1) {
+        printf("The num is NOT prime\n");
+        return 0;
+    }
+
+    int isPrime = 1; // Assume the number is prime (1 = true)
+
+    // Check divisors from 2 up to i * i <= n (equivalent to sqrt(n))
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            isPrime = 0; // Found a divisor, so it's not prime
+            break;       // Exit the loop early to save time
+        }
+    }
+
+    if (isPrime) {
+        printf("The num is prime\n");
+    } else {
+        printf("The num is NOT prime\n");
+    }
+
+    return 0;
+} */
+
